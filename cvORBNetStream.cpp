74ad062ca639cf37765ec2daee63df7d85d6d867
KamiliArsyad <arsyad.ik@gmail.com>
2023-06-20 13:46:39 +0800

    Initialization (copy and few name changes from prev repo)

---
+++ cvORBNetStream.cpp
@@ -0,0 +1,86 @@
+#include "VPINetStream.h"
+#include <iostream>
+
+cvORBNetStream::cvORBNetStream()
+{
+  // Set up the ZMQ context
+  context = zmq::context_t(1);
+}
+
+cvORBNetStream::~cvORBNetStream()
+{
+  socket.close();
+}
+
+/**
+ * Quick note regarding both Init functions: You might notice that Init() can be called however many times
+ * you want, this is because I've yet to thought of a way to make a proper C++ class that has a nice
+ * factory method or perhaps different subclasses; am not used with OOP with C++ yet and I don't intend
+ * to spend too much time on this.
+ */
+
+int cvORBNetStream::Init(int port)
+{
+  std::cout << "Initializing worker stream on port " << port << std::endl;
+  assert(this->port == 0); // Make sure the port hasn't been set yet (can't reinitialize)
+  this->port = port;
+  socket = zmq::socket_t(context, zmq::socket_type::rep);
+  socket.bind("tcp://*:" + std::to_string(port));
+  return 0;
+}
+
+int cvORBNetStream::Init(std::string ip, int port)
+{
+  assert(this->port == 0); // Make sure the port hasn't been set yet (can't reinitialize)
+  this->ip = ip;
+  this->port = port;
+  socket = zmq::socket_t(context, zmq::socket_type::req);
+  socket.connect("tcp://" + ip + ":" + std::to_string(port));
+  return 0;
+}
+
+int cvORBNetStream::SendFrame(std::string encodedFrame)
+{
+  zmq::message_t request(encodedFrame.size());
+  memcpy(request.data(), encodedFrame.c_str(), encodedFrame.size());
+
+  // TODO: What if there's no request? Add a buffer with a safe-distancing semaphore.
+  try
+  {
+    zmq::message_t temp;
+    socket.recv(temp, zmq::recv_flags::dontwait);
+    socket.send(request, zmq::send_flags::dontwait);
+  }
+  catch (zmq::error_t e)
+  {
+    std::cout << "Error sending frame: " << e.what() << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
+
+FrameData cvORBNetStream::ReceiveFrame()
+{
+  zmq::message_t reply;
+  try
+  {
+    zmq::message_t request(1);
+    memcpy(request.data(), "1", 1);
+    socket.send(request, zmq::send_flags::none);
+
+    socket.recv(reply, zmq::recv_flags::none);
+  }
+  catch (zmq::error_t e)
+  {
+    std::cout << "Error receiving frame: " << e.what() << std::endl;
+    FrameData frameData;
+    frameData.frameNumber = -1;
+    return frameData;
+  }
+
+  std::string encodedFrame = std::string(static_cast<char*>(reply.data()), reply.size());
+  FrameData frameData = DecodeKeypoints(encodedFrame);
+  return frameData;
+}
+
---
+++ cvORBNetStream.h
@@ -0,0 +1,78 @@
+#include <vpi/Array.h>
+#include <vpi/Stream.h>
+
+#include <bitset>
+#include <string>
+
+#include <zmq.hpp>
+
+/**
+ * Struct to hold the data of a single frame.
+ */
+struct FrameData
+{
+  int frameNumber;
+  int numKeypoints; 
+  std::bitset<256> *descriptors;
+  VPIKeypointF32 *keypoints; 
+};
+
+/**
+ * Function to encode the keypoints and their respective descriptors of a frame
+ * into a single string. The format is as follows:
+ * <frameNumber>;<numKeypoints>;<desc1>;<x1>,<y1>;<desc2>;<x2>,<y2>;...
+ * The descriptors are encoded as 32 characters ASCII strings for efficiency.
+ */
+std::string EncodeKeypoints(VPIArray keypointsArray, VPIArray descriptorsArray, int numKeypoints, int frameNumber);
+
+
+/**
+ * Function to decode the keypoints and their respective descriptors from an encoded string
+ * into a FrameData struct.
+*/
+FrameData DecodeKeypoints(std::string encodedKeypoints);
+
+
+/**
+ * Object to handle all the streaming operations.
+ */
+class cvORBNetStream
+{
+  private:
+    zmq::context_t context;
+    zmq::socket_t socket;
+    int port = 0;
+    std::string ip;
+  public:
+    cvORBNetStream();
+    ~cvORBNetStream();
+
+
+    /**
+     * Initialize an edge/node/worker stream.
+     * @param port The port to listen to.
+     * @return 0 if successful, -1 otherwise.
+     */
+    int Init(int port);
+
+    /**
+     * Initialize a cloud/processing stream.
+     * @param ip The ip to connect to.
+     * @param port The port to connect to.
+     * @return 0 if successful, -1 otherwise.
+     */
+    int Init(std::string ip, int port);
+
+    /**
+     * Send an encoded frame. 
+     * @param encodedFrame The encoded frame to send.
+     * @return 0 if successful, -1 otherwise.
+     */
+    int SendFrame(std::string encodedFrame);
+
+    /**
+     * Function to receive a frame.
+     * @return The received frame as FrameData struct.
+     */
+    FrameData ReceiveFrame();
+};
